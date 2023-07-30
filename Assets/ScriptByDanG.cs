2a2d30470a94c44e7dc89078ecb860cb42e9081a
DanielPaulGriffin <59080776+DanielPaulGriffin@users.noreply.github.com>
2023-07-31 09:44:18 +1000

    Initial commit

---
+++ Assets/ScriptByDanG.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptByDanG : MonoBehaviour
+{
+    //Sup Dan? you should try and add another debug log to this script, and maybe create a similar script of your own for me to append to :)
+    //Then we gotta figure out how to merge the changes?
+
+    void Start()
+    {
+        Debug.Log("This DanG script just prints this log :)");
+    }
+}
